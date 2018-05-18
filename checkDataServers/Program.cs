b83e00399a046e8fee82d1e9a2455f6086977787
David ROCHELET <drochelet@datasolution.fr>
2018-05-18 10:04:22 +0200

    Import fonction de test

--- checkDataServers/Program.cs
+++ checkDataServers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using 
+
 
 namespace checkDataServers
 {
@@ -6,7 +8,33 @@ namespace checkDataServers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Vérification du status des sites de test");
+
+
+        }
+
+        public override bool WebSiteIsAvailable(string Url)
+        {
+            string Message = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(Url);
+
+            // Set the credentials to the current user account
+            request.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            request.Method = "GET";
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    // Do nothing; we're only testing to see if we can get the response
+                }
+            }
+            catch (WebException ex)
+            {
+                Message += ((Message.Length > 0) ? "\n" : "") + ex.Message;
+            }
+
+            return (Message.Length == 0);
         }
     }
 }
